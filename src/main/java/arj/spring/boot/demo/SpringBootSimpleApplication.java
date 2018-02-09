df83e348745f278b09bf2629a4c9f9a836cf5c0f
Arjun Prasad <Arjun.Prasad@intelematics.com>
2018-02-09 15:19:53 +1100

    Ininitialized simple spring boot app

---
+++ src/main/java/arj/spring/boot/demo/SpringBootSimpleApplication.java
@@ -0,0 +1,12 @@
+package arj.spring.boot.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootSimpleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootSimpleApplication.class, args);
+	}
+}
