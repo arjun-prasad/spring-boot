510dc9baf45767db40907e4b92033b389c6e6fbd
arjun <droid.arj@gmail.com>
2020-08-10 13:53:49 +0530

    Added ServletInitializer

--- src/main/java/arj/spring/boot/demo/SpringBootSimpleApplication.java
+++ src/main/java/arj/spring/boot/demo/SpringBootSimpleApplication.java
@@ -1,12 +1,22 @@
 package arj.spring.boot.demo;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootSimpleApplication {
+public class SpringBootSimpleApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return configureApplication(builder);
+	}
+	
+	private static SpringApplicationBuilder configureApplication(SpringApplicationBuilder builder) {
+		return builder.sources(SpringBootSimpleApplication.class);
+	}
+	
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSimpleApplication.class, args);
+		configureApplication(new SpringApplicationBuilder()).run(args);
 	}
 }
